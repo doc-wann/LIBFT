8745f75c972e0c0477cea55e7d15869353d04dea
Henrique Danielewicz <106987431+doc-wann@users.noreply.github.com>
2022-09-30 18:51:42 -0300

    Update libft.h

--- libft.h
+++ libft.h
@@ -37,7 +37,7 @@ void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *dst, const void *src, size_t len);
 void	*ft_memset(void *s, int c, size_t n);
 char	*ft_strchr(const char *str, int c);
-char	*ft_strdup(char *s);
+char	*ft_strdup(const char *s);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
 size_t	ft_strlcpy(char *dest, const char *src, size_t size);
 int		ft_strlen(const char *s);
